04747cb651309445368bef0965342e53b625f687
Themba Real Lukhele <realthemba@gmail.com>
2022-08-10 16:45:50 +0200

    from this point I am creating a field to enter a video link I wish to download

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ root.title("Lukhele's - youtube downloader")
 
 Label(root,text= 'Youtube Video Downloader', font='arial 20 bold').pack()
 
+
+#from this point I am creating a field to enter a video link I wish to download
+
+link = StringVar()
+
+Label(root, text = 'Paste URL here:', font = 'arial 15 bold').place(x = 160, y = 60)
+link_enter = Entry(root, width = 70, textvariable = link).place(x =32, y = 90)
